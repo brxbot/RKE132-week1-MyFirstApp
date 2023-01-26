9e20a413ddbb3627b99cb0da47a30484bff0c8e8
brxbot#git config --global user.email #birger.botker@gmail.com <notme@Triinu.local>
2023-01-26 14:28:51 +0100

    Greate new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// This is a cooment to my code
+
+// rakendus küsib kastuajal sisestada tema nimi
+// rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name:");
+// strin -sõne
+string userName;
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello " +userName + "!");
+
+//strind interpolation
+
+Console.WriteLine($"Hello, {userName}!");
+
